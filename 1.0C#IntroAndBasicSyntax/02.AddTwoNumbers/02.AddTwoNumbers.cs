c4e7ebde58a2cfdb582befd07d8f5c02bc788edd
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-02 19:31:18 +0100

    C# Intro and Basic Syntax

---
+++ 1.0C#IntroAndBasicSyntax/02.AddTwoNumbers/02.AddTwoNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02.AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a =int.Parse(Console.ReadLine());
+            var b =int.Parse(Console.ReadLine());
+            var sum = a + b;
+            Console.WriteLine($"{ a} + {b} = {sum}" );
+        }
+    }
+}
---
+++ 1.0C#IntroAndBasicSyntax/03.EmployeeData/03.EmployeeData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03.EmployeeData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int employeedId = int.Parse(Console.ReadLine());
+            decimal salary = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Employee ID:{employeedId:D8}");
+            Console.WriteLine($"Salary: {salary:f2}");
+        }
+    }
+}

9e05c2b77952ab938aa033b8e007f57a4aee110e
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-04 12:33:40 +0100

    Data Types and Variables - Lab

---
+++ 3.0DataTypesAndVariablesLab/02.CircleArea(Precision 12)/02.CircleArea(Precision 12).cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _02.CircleArea_Precision_12_
+{
+    class Program
+    {
+        static void Main()
+        {
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{Math.PI * radius * radius:f12}");
+        }
+    }
+}

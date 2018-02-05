eed3a8d324fba7e567db9259d5bd6297c0bbd0a7
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-05 18:23:27 +0100

    Data Types and Variables - Lab

---
+++ 3.0DataTypesAndVariablesLab/09.RefactorSpecialNumbers/09.RefactorSpecialNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _09.RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int currentNumber = 0;
+            int digitSum = 0;
+
+            for (int i = 1; i <=n; i++)
+            {
+                currentNumber = i;
+
+                while (i > 0)
+                {
+                    digitSum += i % 10;
+                    i = i / 10;
+                }
+
+                bool magicNumber = (digitSum == 5) || (digitSum == 7) || (digitSum == 11);
+
+                Console.WriteLine($"{currentNumber} -> {magicNumber}");
+
+                digitSum = 0;
+                i = currentNumber;
+            }
+        }
+    }
+}

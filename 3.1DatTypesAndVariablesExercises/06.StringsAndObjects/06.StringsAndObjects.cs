f8cd1741dc69a45d733c932d2cee8e84420a162a
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-06 21:38:04 +0100

    Data Types And Variables - Exercises

---
+++ 3.1DatTypesAndVariablesExercises/06.StringsAndObjects/06.StringsAndObjects.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _06.StringsAndObjects
+{
+    class Program
+    {
+        static void Main()
+        {
+            string firstWord = "Hello";
+            string secondWord = "World";
+            object sentence = firstWord + " " + secondWord;
+            string completeSentence = (string)sentence;
+            Console.WriteLine(completeSentence);
+        }
+    }
+}
---
+++ 3.1DatTypesAndVariablesExercises/09.ReversedChars/09.ReversedChars.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _09.ReversedChars
+{
+    class Program
+    {
+        static void Main()
+        {
+            char firstCharacter = char.Parse(Console.ReadLine());
+            char secondCharacter = char.Parse(Console.ReadLine());
+            char thirdCharacter = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{thirdCharacter}{secondCharacter}{firstCharacter}");
+        }
+    }
+}

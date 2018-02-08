0c4f64a2abe4462c94f25dbd7d1afa617bc3d2ee
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-08 14:40:23 +0100

    Data Types and Variables - Exercises

---
+++ 3.1DatTypesAndVariables-Exercises/12.RectangleProperties/12.RectangleProperties.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _12.RectangleProperties
+{
+    class Program
+    {
+        static void Main()
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double diagonal = Math.Sqrt(width * width + height * height);
+            double area = width * height;
+            double perimeter = (width + height) * 2;
+
+            Console.WriteLine(perimeter);
+            Console.WriteLine(area);
+            Console.WriteLine(diagonal);
+        }
+    }
+}

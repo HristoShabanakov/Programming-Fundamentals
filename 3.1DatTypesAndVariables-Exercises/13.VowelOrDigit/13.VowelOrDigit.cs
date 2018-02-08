73c4368083fdf7cfc8ae0f325e17301190ada187
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-08 19:17:39 +0100

    Data Types and Variables - Exercises

---
+++ 3.1DatTypesAndVariables-Exercises/13.VowelOrDigit/13.VowelOrDigit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _13.VowelOrDigit
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            try
+            {
+                int number = int.Parse(input);
+                Console.WriteLine("digit");
+            }
+            catch (Exception)
+            {
+
+                if ("aoueiy".IndexOf(input) >= 0) Console.WriteLine("vowel");
+                else Console.WriteLine("other");
+            }
+        }
+    }
+}

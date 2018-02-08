c670dfafe92ad5751c2bf5f9bc548cbee6687f20
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-08 21:52:12 +0100

    Data Types and Variables - Exercises

---
+++ 3.1DatTypesAndVariables-Exercises/14.IntegerToHexAndBinary/14.IntegerToHexAndBinary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.IntegerToHexAndBinary
+{
+    class Program
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            
+            string result = Convert.ToString(input,16).ToUpper();
+            string binary = Convert.ToString(input, 2).ToUpper();
+            Console.WriteLine(result);
+            Console.WriteLine(binary);
+        }
+    }
+}

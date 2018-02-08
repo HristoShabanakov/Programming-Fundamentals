eb1537f6a1cd3c1d113f3ee4eec93e8061e4afc5
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-08 22:31:53 +0100

    Data Types and Variables - Exercises

---
+++ 3.1DatTypesAndVariables-Exercises/15.FastPrimeChecker/15.FastPrimeChecker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _15.FastPrimeChecker
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            //A prime number has no divisors (other than itself and 1). 
+            for (int i = 2; i <= number; i++)
+            {
+                bool isItPrime = true;
+                for (int j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if(i % j == 0)
+                    {
+                        isItPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{i} -> {isItPrime}");
+            }
+        }
+    }
+}

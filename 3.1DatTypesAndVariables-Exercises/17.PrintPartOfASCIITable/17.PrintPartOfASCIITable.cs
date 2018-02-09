075d5b3f615c3e944dac4431cc086fd7ba881af1
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-09 19:22:37 +0100

    Data Types and Variables - Exercises

---
+++ 3.1DatTypesAndVariables-Exercises/17.PrintPartOfASCIITable/17.PrintPartOfASCIITable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _17.PrintPartOfASCIITable
+{
+    class Program
+    {
+        static void Main()
+        {
+            int firstChar = int.Parse(Console.ReadLine());
+            int secondChar = int.Parse(Console.ReadLine());
+
+            for (int i = firstChar; i <= secondChar; i++)
+            {
+                Console.Write((char)(i) +" " );
+            }
+        }
+    }
+}

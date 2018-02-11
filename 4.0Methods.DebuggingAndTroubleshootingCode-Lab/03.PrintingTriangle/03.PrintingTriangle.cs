d1700a7d5aaee5fe876fdf4c55700491c963f8d2
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-11 23:02:22 +0100

    Methods. Debugging and Troubleshooting Code - Lab

---
+++ 4.0Methods.DebuggingAndTroubleshootingCode-Lab/03.PrintingTriangle/03.PrintingTriangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _03.PrintingTriangle
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            PrintTriangle(number);
+        }
+
+        static void PrintLine(int start, int end) //Creating a method which prints a single line of the figure
+        {
+            for (int i = start; i <=end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void PrintTriangle(int n) 
+        //Creating a second method which prints the first half of the figure with one for loop,
+        //and then the second part of the figure with second for loop.
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                PrintLine(1, i);
+            }
+
+            for (int i = n-1; i >= 1; i--)
+            {
+                PrintLine(1, i);
+            }
+        }
+    }
+}

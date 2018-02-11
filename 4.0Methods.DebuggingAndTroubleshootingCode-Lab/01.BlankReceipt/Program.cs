29c91de994bb914cca48e29ff9454706397feddf
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-11 22:23:48 +0100

    Methods. Debugging and Troubleshooting Code - Lab

---
+++ 4.0Methods.DebuggingAndTroubleshootingCode-Lab/01.BlankReceipt/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _012.BlankReceipt
+{
+    class Program
+    {
+        static void Main()
+        {
+            PrinteReceipt();
+        }
+        static void PrintHeader()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+        static void PrintMiddle()
+        {
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+        }
+        static void PrintFooter()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("\u00A9 SoftUni");
+        }
+        static void PrinteReceipt()
+        {
+            PrintHeader();
+            PrintMiddle();
+            PrintFooter();
+        }
+    }
+}

911415363a51837799e348ff64fbc5342b957d05
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-11 22:24:51 +0100

    Methods. Debugging and Troubleshooting Code - Lab

---
+++ 4.0Methods.DebuggingAndTroubleshootingCode-Lab/02.SignOfIntegerNumber/02.SignOfIntegerNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02.SignOfIntegerNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintInteger(n);
+        }
+        static void PrintInteger (int number)
+        {
+            if(number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if(number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}

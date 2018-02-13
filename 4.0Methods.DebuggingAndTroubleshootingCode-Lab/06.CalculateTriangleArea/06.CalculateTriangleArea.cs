00fdf9e6f2b413255e5be3e918be21181e382b07
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-13 17:10:57 +0100

    Methods. Debugging and Troubleshooting Code - Lab

---
+++ 4.0Methods.DebuggingAndTroubleshootingCode-Lab/06.CalculateTriangleArea/06.CalculateTriangleArea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _06.CalculateTriangleArea
+{
+    class Program
+    {
+        static void Main()
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = CalculateTriangleArea(width, height);
+            Console.WriteLine(area);
+        }
+
+        static double CalculateTriangleArea(double width, double height)
+        {
+            return width * height / 2;
+        }
+    }
+}

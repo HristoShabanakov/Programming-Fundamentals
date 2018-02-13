9c682405752770c9f8bdbe52bf519d253779e3fd
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-13 16:31:04 +0100

    Methods. Debugging and Troubleshooting Code - Lab

---
+++ 4.0Methods.DebuggingAndTroubleshootingCode-Lab/05.TemperatureConversion/05.TemperatureConversion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _05.TemperatureConversion
+{
+    class Program
+    {
+        static void Main()
+        {
+            double fahrenheit = double.Parse(Console.ReadLine());
+            double celsius = FahrenheitToCelsius(fahrenheit); //Invoking the method and saving its return value in new variable.
+            Console.WriteLine($"{celsius:f2}");
+        }
+
+        static double FahrenheitToCelsius(double fahrenheit) //Creating method which returns a value of type double.
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
+    }
+}

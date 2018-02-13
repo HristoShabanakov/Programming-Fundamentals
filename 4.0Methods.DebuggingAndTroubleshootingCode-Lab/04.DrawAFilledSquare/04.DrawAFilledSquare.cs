73be4c26fb8d5efa0320b7f33d1a04755010813e
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-13 16:07:45 +0100

    Methods. Debugging and Troubleshooting Code - Lab

---
+++ 4.0Methods.DebuggingAndTroubleshootingCode-Lab/04.DrawAFilledSquare/04.DrawAFilledSquare.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04.DrawAFilledSquare
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            DrawHeader(n);
+            DrawMiddlePart(n);
+            DrawFooter(n);
+        }
+
+        static void DrawHeader(int n)
+        {
+            Console.WriteLine(new string('-', n * 2));
+        }
+
+        static void DrawMiddlePart(int n)
+        {
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write('-');
+                for (int j = 1; j < n; j++)
+                {
+                    Console.Write("\\/");
+                }
+                Console.WriteLine('-');
+            }
+        }
+
+        static void DrawFooter(int n)
+        {
+            Console.WriteLine(new string('-', n * 2));
+        }
+    }
+}

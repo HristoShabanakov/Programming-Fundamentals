d491aa08bd2a1a07702717ae9a2a6e68c80176e1
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-14 18:51:40 +0100

    Methods. Debugging and Troubleshooting Code - Lab

---
+++ 4.0Methods.DebuggingAndTroubleshootingCode-Lab/10.HolidaysBetweenTwoDates/10.HolidaysBetweenTwoDates.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace _10.HolidaysBetweenTwoDates
+{
+    class Program
+    {
+        static void Main()
+        {
+            var startDate = DateTime.ParseExact(Console.ReadLine(), 
+            "d.M.yyyy", CultureInfo.InvariantCulture);
+        //Broken code was : "dd.m.yyyy"
+            var endDate = DateTime.ParseExact(Console.ReadLine(),
+                "d.M.yyyy", CultureInfo.InvariantCulture);
+        //Broken code was : "dd.m.yyyy"
+            var holidaysCount = 0;
+
+            for (var date = startDate; date <= endDate; date = date.AddDays(1))
+          //Broken code : "date =" - was missing.
+
+                if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+                    holidaysCount++;
+         //Broken code  : "||" was replaced by "&&"
+            Console.WriteLine(holidaysCount);
+        }
+    }
+}

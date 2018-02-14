ff677af6009ec606c897be1e6fba462a45d15538
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-14 18:44:10 +0100

    Methods. Debugging and Troubleshooting Code - Lab

---
+++ 4.0Methods.DebuggingAndTroubleshootingCode-Lab/09.MultiplyEvensByOdds/09.MultiplyEvensByOdds.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _09.MultiplyEvensByOdds
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+            int result = GetMultipleOfEvenAndOdds(number);
+            Console.WriteLine(result);
+        }
+
+        static int GetMultipleOfEvenAndOdds(int number)
+        {
+            int sumEvens = GetSumOfEvenDigits(number);
+            int sumOdds = GetSumOfOddDigits(number);
+            return sumEvens * sumOdds;
+        }
+
+        private static int GetSumOfOddDigits(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                int lastDigit = number % 10;
+                if(lastDigit % 2 != 0)
+                {
+                    sum +=lastDigit;
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+
+        private static int GetSumOfEvenDigits(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                int lastDigit = number % 10;
+                if(lastDigit % 2 == 0)
+                {
+                    sum += lastDigit;
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+}

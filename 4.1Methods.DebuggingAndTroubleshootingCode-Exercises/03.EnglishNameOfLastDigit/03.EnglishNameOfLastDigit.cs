5d55e76ba2ac6fa73f920d53b12b32f3c3676c7c
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-16 00:36:01 +0100

    Methods. Debugging and Troubleshooting Code - Exercises

---
+++ 4.1Methods.DebuggingAndTroubleshootingCode-Exercises/03.EnglishNameOfLastDigit/03.EnglishNameOfLastDigit.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _03.EnglishNameOfLastDigit
+{
+    class Program
+    {
+        static void Main()
+        {
+            PrintEnglishName();
+        }
+        static void PrintEnglishName()
+        {
+            long input = Math.Abs( long.Parse(Console.ReadLine()));
+            long lastDigit = input % 10;
+            switch (lastDigit)
+            //We can replace the "switch" construction with an "array" of strings.
+            //string[] digitAsWord = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            
+            {
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+                case 2:
+                    Console.WriteLine("two");
+                    break;
+                case 3:
+                    Console.WriteLine("three");
+                    break;
+                case 4:
+                    Console.WriteLine("four");
+                        break;
+                case 5:
+                    Console.WriteLine("five");
+                        break;
+                case 6:
+                    Console.WriteLine("six");
+                    break;
+                case 7:
+                    Console.WriteLine("seven");
+                    break;
+                case 8:
+                    Console.WriteLine("eight");
+                    break;
+                case 9:
+                    Console.WriteLine("nine");
+                    break;
+                case 0:
+                    Console.WriteLine("zero");
+                    break;
+            }
+
+        }
+    }
+}

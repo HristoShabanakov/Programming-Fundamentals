f11b1399d09cf55fbe1b425ded446e58d7c8115a
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-15 10:04:49 +0100

    Methods. Debugging and Troubleshooting Code - Exercises

---
+++ 4.1Methods.DebuggingAndTroubleshootingCode-Exercises/01.HelloName/01.HelloName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01.HelloName
+{
+    class Program
+    {
+        static void Main()
+        {
+            PrintName();
+        }
+
+        static void PrintName()
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
---
+++ 4.1Methods.DebuggingAndTroubleshootingCode-Exercises/02.MaxMethod/02.MaxMethod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02.MaxMethod
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            GetMaxOfTwoNumbers();
+        }
+
+         static void GetMaxOfTwoNumbers ()
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            int numberThree = int.Parse(Console.ReadLine());
+            int maxNumber = Math.Max(numberOne, Math.Max(numberTwo,numberThree));
+
+            Console.WriteLine(maxNumber);
+        }
+    }
+}

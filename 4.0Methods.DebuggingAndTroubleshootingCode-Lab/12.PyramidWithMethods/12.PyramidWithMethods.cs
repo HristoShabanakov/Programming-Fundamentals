4842f47afa83b0e2983d75a8385a06450437e70e
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-15 21:15:22 +0100

    Methods. Debugging and Troubleshooting Code - Lab

---
+++ 4.0Methods.DebuggingAndTroubleshootingCode-Lab/12.PyramidWithMethods/12.PyramidWithMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PyramidWithMethods
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+            {
+                PrintNumbers(1, i);
+            }
+            for (int j = n; j >=1; j--)
+            {
+                PrintNumbers(1, j);
+            }
+        }
+        static void PrintNumbers(int start, int end)
+        {
+            for (int i = start; i <=end; i++)
+            {
+                Console.Write(i);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}

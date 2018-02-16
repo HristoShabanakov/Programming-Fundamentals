429c503a552e04ac4bfd1ba7d644efde3fd6d889
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-16 21:41:12 +0100

    Methods. Debugging and Troubleshooting Code - Exercises

---
+++ 4.1Methods.DebuggingAndTroubleshootingCode-Exercises/04.NumbersInReversedOrder/04.NumbersInReversedOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.NumbersInReversedOrder
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(ReverseDecimalNumber(input));
+        }
+        static string ReverseDecimalNumber(string number)
+        {
+            string reverseNumber = string.Join("", number.Reverse());
+            return reverseNumber;
+        }
+    }
+}

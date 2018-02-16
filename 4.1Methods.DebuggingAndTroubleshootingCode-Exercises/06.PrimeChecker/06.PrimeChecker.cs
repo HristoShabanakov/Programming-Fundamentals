0d49198ddfe6018b294c1f89c06dac0c3197c99f
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-16 22:20:14 +0100

    Methods. Debugging and Troubleshooting Code - Exercises

---
+++ 4.1Methods.DebuggingAndTroubleshootingCode-Exercises/06.PrimeChecker/06.PrimeChecker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _06.PrimeChecker
+{
+    class Program
+    {
+        static void Main()
+        {
+            long input = long.Parse(Console.ReadLine());
+            Console.WriteLine(IsPrime(input));
+        }
+        static bool IsPrime(long n)
+        {
+            if (n <= 1)
+                return false;
+            for (int i = 2; i <=Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}

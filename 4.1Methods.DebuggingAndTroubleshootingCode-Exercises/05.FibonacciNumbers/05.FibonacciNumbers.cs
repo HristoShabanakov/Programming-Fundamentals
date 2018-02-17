b9d5c252941435ffea7fb42e6420857cc62cb60a
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-17 23:58:32 +0100

    Methods. Debugging and Troubleshooting Code - Exercises

---
+++ 4.1Methods.DebuggingAndTroubleshootingCode-Exercises/05.FibonacciNumbers/05.FibonacciNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05.FibonacciNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(FibonacciNumbers(number));
+        }
+        static int FibonacciNumbers(int n)
+        {
+
+            int a = 0;
+            int b = 1;
+
+            for (int i = 0; i < n; i++)
+            // Fibonacci numbers are a useful pattern.
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+             //By storing the previous value in a loop iteration, we avoid computational work.
+            }
+            return b;
+        }
+    }
+}

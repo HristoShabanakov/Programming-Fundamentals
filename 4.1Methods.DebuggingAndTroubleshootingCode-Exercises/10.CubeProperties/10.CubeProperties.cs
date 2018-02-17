04f89f2c824c4f6f18dd47649888ae80452ebee2
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-17 23:18:07 +0100

    Methods. Debugging and Troubleshooting Code - Exercises

---
+++ 4.1Methods.DebuggingAndTroubleshootingCode-Exercises/10.CubeProperties/10.CubeProperties.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _10.CubeProperties
+{
+    class Program
+    {
+        static void Main()
+        {
+            double side = double.Parse(Console.ReadLine());
+            string parameter = Console.ReadLine();
+            switch (parameter)
+            {
+                case "face":
+                    GetCubeLengthOfFaceDiagonals(side);
+                    break;
+                case "volume":
+                    GetCubeVolume(side);
+                    break;
+                case "space":
+                    GetCubeSpaceDiagonals(side);
+                    break;
+                case "area":
+                    GetCubeArea(side);
+                    break;
+            }
+        }
+        static void GetCubeArea(double side)
+        {
+            double area = 6 * Math.Pow(side,2);
+            Console.WriteLine($"{area:f2}");
+        }
+
+        static void GetCubeVolume(double side)
+        {
+            double volume = Math.Pow(side, 3);
+            Console.WriteLine($"{volume:f2}");
+        }
+
+        static void GetCubeSpaceDiagonals (double side)
+        {
+            double spaceDiagonals = Math.Sqrt(3) * side;
+            Console.WriteLine($"{spaceDiagonals:f2}");
+        }
+        static void GetCubeLengthOfFaceDiagonals(double side)
+        {
+            double lengthFaceDiagonals = Math.Sqrt(2) * side;
+            Console.WriteLine($"{lengthFaceDiagonals:f2}");
+        }
+    }
+}

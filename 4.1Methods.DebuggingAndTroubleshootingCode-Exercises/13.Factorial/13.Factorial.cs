e045a4fa1e8d0b1961a7d41930eba18a14982be6
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-18 21:53:27 +0100

    Methods. Debugging and Troubleshooting Code - Exercises

---
+++ 4.1Methods.DebuggingAndTroubleshootingCode-Exercises/13.Factorial/13.Factorial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+
+namespace _13.Factorial
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+        }
+        public static BigInteger Factorial(int factorial)
+        {
+            BigInteger factorialNumber = 1;
+         //A factorial is a function that multiplies a number by every number below it. For example 5!= 5*4*3*2*1 = 120.
+
+            for (int i = 1; i <= factorial; i++)
+            {
+                factorialNumber = factorialNumber * i;
+            }
+
+            return factorialNumber;
+        }
+    }
+}

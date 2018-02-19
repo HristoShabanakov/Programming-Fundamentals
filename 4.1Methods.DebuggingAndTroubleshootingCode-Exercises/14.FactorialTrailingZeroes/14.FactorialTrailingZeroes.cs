02912b20b66d87d6d56cd3825eefaa538823464e
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-19 23:52:47 +0100

    Methods. Debugging and Troubleshooting Code - Exercises

---
+++ 4.1Methods.DebuggingAndTroubleshootingCode-Exercises/14.FactorialTrailingZeroes/14.FactorialTrailingZeroes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace _14.FactorialTrailingZeroes
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            int trailingZeros = GetTrailingZeros(number);
+
+            Console.WriteLine(trailingZeros);
+
+        }
+
+
+        public static int GetTrailingZeros(int number)
+        {
+
+            int count = 0;
+            for (int i = 5; number / i >= 1; i *= 5)
+            {
+                count += number / i;
+            }
+            return count;
+        }
+    }
+}

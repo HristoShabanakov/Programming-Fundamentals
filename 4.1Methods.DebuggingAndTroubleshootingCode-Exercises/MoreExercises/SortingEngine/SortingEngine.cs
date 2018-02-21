827ad11b8adac01f5561bc5c21c923008b2a9c99
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-21 09:33:54 +0100

    Methods - more exercises

---
+++ 4.1Methods.DebuggingAndTroubleshootingCode-Exercises/MoreExercises/SortingEngine/SortingEngine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SortingEngine
+{
+    class SortingEngine
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Sort(10, 3, 2, -7, 18, 55, 48);
+            PrintNumbers(numbers);
+        }
+
+        static int[] Sort(params int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                for (int j = 1 +i; j < numbers.Length; j++)
+                {
+                    if(numbers[i] > numbers[j])
+                    {
+                        int oldNum = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = oldNum;
+                    }
+                }
+            }
+            return numbers;
+        }
+
+        static void PrintNumbers(params int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]}");
+                if (i < (numbers.Length - 1))
+                {
+                    Console.Write(", ");
+                }
+            }
+        }
+    }
+}

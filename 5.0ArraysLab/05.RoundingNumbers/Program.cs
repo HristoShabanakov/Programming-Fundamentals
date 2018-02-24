793a05e6d5f4610cdc14829e50923a03ae3e4e7a
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-02-24 22:55:28 +0100

    Arrays - Lab

---
+++ 5.0ArraysLab/05.RoundingNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.RoundingNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            double[] numbers = Console.ReadLine().
+                Split(' ').
+                Select(double.Parse).
+                ToArray();
+
+            foreach (var number in numbers)
+            {
+                double rounded = Math.Round(number, 0, MidpointRounding.AwayFromZero);
+                Console.WriteLine(number + " =>" + rounded);
+            }
+        }
+    }
+}

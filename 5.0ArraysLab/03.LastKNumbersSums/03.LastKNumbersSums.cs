e7b5e46751642e363c6d4dfd883ad81da398b247
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-03-04 22:52:43 +0100

    Arrays - Lab

---
+++ 5.0ArraysLab/03.LastKNumbersSums/03.LastKNumbersSums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.LastKNumbersSums
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            int[] sequence = new int[n];
+            sequence[0] = 1;
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = i-1; j >=Math.Max(0, i-k); j--)
+                {
+                    sequence[i] += sequence[j];
+                }
+            }
+
+            Console.WriteLine(string.Join(" ",sequence));
+        }
+    }
+}

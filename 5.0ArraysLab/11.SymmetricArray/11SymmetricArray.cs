390e072cab23baca51a074fe809afe33ad14aee9
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-03-04 22:55:53 +0100

    Arrays - Lab

---
+++ 5.0ArraysLab/11.SymmetricArray/11SymmetricArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _11.SymmetricArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            bool symmetric = true;
+
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                if (array[i] != array[n - i - 1])
+                {
+                    symmetric = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Is symmetric? {symmetric}");
+        }
+    }
+}

0c57ea3e72224c782380abac52ed34a8688b5e64
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-03-04 23:59:18 +0100

    Arrays - Lab

---
+++ 5.0ArraysLab/06.ReverseArrayOfStrings/06.ReverseArrayOfStrings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.ReverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string[] array = Console.ReadLine().Split(' ').ToArray();
+
+            Array.Reverse(array);
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}

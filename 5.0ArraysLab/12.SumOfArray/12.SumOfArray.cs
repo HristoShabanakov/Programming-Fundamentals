b37295dbd0ad03951dd168e6b6d36f0456eceeb6
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-03-04 22:56:54 +0100

    Arrays - Lab

---
+++ 5.0ArraysLab/12.SumOfArray/12.SumOfArray.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _12.SumOfArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] items = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Console.WriteLine(items.Sum());
+        }
+    }
+}

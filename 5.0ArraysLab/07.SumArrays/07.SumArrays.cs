fa0c768ca93644abbd728c4674c5a9910328a957
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-03-05 23:40:27 +0100

    Arrays - Lab

---
+++ 5.0ArraysLab/07.SumArrays/07.SumArrays.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.SumArrays
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] firstArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] secondArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int maxLenght = Math.Max(firstArray.Length, secondArray.Length);
+            int [] sumOfArrays = new int[maxLenght];
+
+            for (int i = 0; i < sumOfArrays.Length; i++)
+            {
+                sumOfArrays[i] = firstArray[i % firstArray.Length] + secondArray[i % secondArray.Length];
+            }
+
+            Console.WriteLine(string.Join(" ", sumOfArrays));
+        }
+    }
+}

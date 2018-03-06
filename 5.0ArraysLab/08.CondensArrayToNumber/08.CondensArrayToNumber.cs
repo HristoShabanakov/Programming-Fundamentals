ad98ea5a7aae7e3cae54b4438cb6d68d67025a95
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-03-06 23:19:33 +0100

    Arrays - Lab

---
+++ 5.0ArraysLab/08.CondensArrayToNumber/08.CondensArrayToNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.CondensArrayToNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            
+
+            while (numbers.Length > 1)
+            {
+                int[] condensed = new int[numbers.Length - 1];
+
+                for (int i = 0; i < condensed.Length; i++)
+                {
+                    condensed[i] = numbers[i] + numbers[i + 1];
+                    
+                }
+                numbers = condensed;
+            }
+            
+
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+    }
+}

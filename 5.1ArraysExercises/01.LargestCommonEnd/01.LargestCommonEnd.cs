16bb7f1f3e68e0b2ce9bb30b6a6c0e718f8e4485
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-03-10 23:45:02 +0100

    Arrays - Exercises

---
+++ 5.1ArraysExercises/01.LargestCommonEnd/01.LargestCommonEnd.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.LargestCommonEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayOfWordsA = Console.ReadLine().Split(' ').ToArray();
+            string[] arrayOfWordsB = Console.ReadLine().Split(' ').ToArray();
+
+            double shorterArray = Math.Min(arrayOfWordsA.Length, arrayOfWordsB.Length);
+            //Math Min - returns the smaller of the arguments.
+
+            int leftCount = 0;
+            int rightCount = 0;
+
+            for (int i = 0; i < shorterArray; i++)
+             //We use the shorterarray, because if we use some of the input arrays we might get OutOfRangeException.
+            {
+                if (arrayOfWordsA[i] == arrayOfWordsB[i])
+                {
+                    leftCount++;
+                }
+                if (arrayOfWordsA[arrayOfWordsA.Length-i-1] == arrayOfWordsB[arrayOfWordsB.Length-i-1])
+                    //"arraysOfWordsA.Length - i - 1" - takes the last element of the array 
+                    //"i" - is taking the next last element with each loop
+                {
+                    rightCount++;
+                }
+            }
+
+            Console.WriteLine(Math.Max(leftCount,rightCount));
+            //Math Max - returns the larger of the two arguments.
+        }
+    }
+}

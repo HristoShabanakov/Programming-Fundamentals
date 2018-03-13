e7a295fb31b72787106f6b117cc95a65aef9931a
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-03-13 23:52:20 +0100

    Arrays - Exercises

---
+++ 5.1ArraysExercises/02.RotateAndSum/02.RotateAndSum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.RotateAndSum
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] arrayOfNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int rotate = int.Parse(Console.ReadLine());
+
+            int[] sum = new int[arrayOfNumbers.Length];
+
+            for (int i = 0; i < rotate; i++)
+            {
+                RotateArrya(arrayOfNumbers);
+
+                for (int j = 0; j < sum.Length; j++)
+                {
+                    sum[j] = sum[j] + arrayOfNumbers[j];
+                }
+            }
+
+            Console.WriteLine(string.Join(" ",sum));
+        }
+
+        private static void RotateArrya(int[] arrayOfNumbers)
+        {
+            int lastNumber = arrayOfNumbers.Last();
+
+            for (int i = arrayOfNumbers.Length-1; i > 0; i--)
+            {
+                arrayOfNumbers[i]=arrayOfNumbers[i-1];
+            }
+            arrayOfNumbers[0] = lastNumber;
+        }
+    }
+}

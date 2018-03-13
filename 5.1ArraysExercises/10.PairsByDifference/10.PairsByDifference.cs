7fce0644b763585c9434008840b3773773d40139
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-03-13 23:54:34 +0100

    Arrays - Exercises

---
+++ 5.1ArraysExercises/10.PairsByDifference/10.PairsByDifference.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _10.PairsByDifference
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int difference = int.Parse(Console.ReadLine());
+
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    int secondNumber = numbers[j];
+                    if (currentNumber - secondNumber == difference)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}

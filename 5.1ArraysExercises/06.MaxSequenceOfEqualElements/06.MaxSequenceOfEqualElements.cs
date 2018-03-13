2eb71c0484e19ef734d3de5a83ec25db4e60d418
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-03-13 23:53:45 +0100

    Arrays - Exercises

---
+++ 5.1ArraysExercises/06.MaxSequenceOfEqualElements/06.MaxSequenceOfEqualElements.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int count = 1;
+            int bestcount = 0;
+            int bestIndex = 0;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] == numbers[i - 1]) 
+               //we check if the previous number is equal to current number
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 1;
+                }
+
+                if (count > bestcount)
+                {
+                    bestcount = count;
+                    bestIndex = i - count + 1;
+                }
+            }
+
+            for (int i = bestIndex; i < bestIndex + bestcount; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+    }
+}

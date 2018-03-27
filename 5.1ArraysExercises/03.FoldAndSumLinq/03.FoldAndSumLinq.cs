a54bf026dd0971dd8e661e642528ce755994b05e
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-03-27 20:36:55 +0200

    Arrays - Exercises

---
+++ 5.1ArraysExercises/03.FoldAndSumLinq/03.FoldAndSumLinq.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.FoldAndSumLinq
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] input = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int length = input.Length;
+            int k = length / 4;
+            int[] firstSetOfNumbers = input.Take(k).Reverse().ToArray();
+            int[] secondSetOfNumbers = input.Reverse().Take(k).ToArray();
+
+            int[] firstRow = firstSetOfNumbers.Concat(secondSetOfNumbers).ToArray();
+            int[] secondRow = input.Skip(k).Take(length / 2).ToArray();
+
+            int[] sum = firstRow.Zip(secondRow, (x, y) => x + y).ToArray();
+            
+            Console.WriteLine(string.Join(" ",sum));
+            
+        }
+    }
+}

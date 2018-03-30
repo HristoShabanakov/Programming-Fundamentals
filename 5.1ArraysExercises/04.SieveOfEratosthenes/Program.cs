0ee70b8cf6b0f02faba6d7078f75ce1530e55f82
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-03-30 21:47:15 +0200

    Arrays - Exercises

---
+++ 5.1ArraysExercises/04.SieveOfEratosthenes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04.SieveOfEratosthenes
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            bool[] arrayOfPrimes = new bool[numbers + 1];
+
+            for (int i = 2; i < arrayOfPrimes.Length; i++)
+            {
+                arrayOfPrimes[i] = true;
+            }
+
+            for (int j = 0; j <=numbers; j++)
+            {
+                if (!arrayOfPrimes[j])
+                {
+                    continue;
+                }
+
+                else
+                {
+                    Console.Write(j +" ");
+                }
+
+                for (int k = 2 * j; k <=numbers; k+=j)
+                {
+                    arrayOfPrimes[k] = false;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}

a27df5c0e1f1f588543c9900aeaa913dac31ef84
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-01 14:38:40 +0200

    Arrays - Lab

---
+++ 5.0ArraysLab/04.TripleSum/04.TripleSum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.TripleSum
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int tripleSum = 0;
+
+            bool containsTriple = false;
+
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    firstNumber = numbers[i];
+                    secondNumber = numbers[j];
+                    tripleSum = firstNumber + secondNumber;
+
+                    if (numbers.Contains(tripleSum))
+                    {
+                        containsTriple = true;
+                        Console.WriteLine($"{firstNumber} + {secondNumber} == {tripleSum}");
+                    }
+                }
+            }
+
+            if (!containsTriple)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}

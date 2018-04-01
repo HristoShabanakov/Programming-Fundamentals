5f4b77e5a0e6c54b46fd45c1743e8adfd0a0c79c
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-01 12:32:31 +0200

    Arrays - Exercises

---
+++ 5.1ArraysExercises/08.MostFrequentNumber/08.MostFrequentNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.MostFrequentNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int frequentNumber = numbers.GroupBy(n => n)
+                .OrderByDescending(n => n.Count())
+                .First()
+                .Key;
+
+            Console.WriteLine(frequentNumber);
+        }
+    }
+}

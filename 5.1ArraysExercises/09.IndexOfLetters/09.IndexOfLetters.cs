55980ec6dcf42d394daa606ce62b0b71bc619f0f
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-01 12:33:50 +0200

    Arrays - Exercises

---
+++ 5.1ArraysExercises/09.IndexOfLetters/09.IndexOfLetters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _09.IndexOfLetters
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] words = Console.ReadLine().ToLower().ToArray();
+
+            foreach (char letter in words)
+            {
+                Console.WriteLine($"{letter} -> {letter - 97}");
+            }
+        }
+    }
+}

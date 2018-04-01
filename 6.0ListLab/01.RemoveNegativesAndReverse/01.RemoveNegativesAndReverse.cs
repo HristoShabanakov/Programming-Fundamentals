31568dc00b0f1cac85a3c20ad6ec2a88aaa96088
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-01 22:21:28 +0200

    Lists - Lab

---
+++ 6.0ListLab/01.RemoveNegativesAndReverse/01.RemoveNegativesAndReverse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).Reverse().ToList();
+            List<int> result = new List<int>();
+
+            foreach (var number in numbers)
+            {
+                if (number > 0)
+                {
+                    result.Add(number);
+                }
+            }
+
+            if (result.Count < 1)
+            {
+                Console.WriteLine("Empty");
+            }
+
+            else
+            {
+                Console.WriteLine(String.Join(" ", result));
+            }
+        }
+    }
+}

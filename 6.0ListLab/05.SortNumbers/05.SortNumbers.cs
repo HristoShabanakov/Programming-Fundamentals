419a9c3350400d9a348b30df2a4a97f2306ecd18
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-02 20:14:41 +0200

    Lists - Lab

---
+++ 6.0ListLab/05.SortNumbers/05.SortNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SortingNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<double> numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToList();
+
+            numbers.Sort();
+
+            Console.WriteLine(String.Join(" <=",numbers));
+
+        }
+    }
+}

5798e078b62a29e1c3b96e0fff7ce73ff06523a2
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-03 20:37:32 +0200

    Lists - Lab

---
+++ 6.0ListLab/07.CountNumbers/07.CountNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.CountNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            SortedDictionary<int,int> counts = new SortedDictionary<int, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!counts.ContainsKey(number))
+                {
+                    counts.Add(number, 0);
+                }
+                counts[number]++;
+            }
+
+            foreach (var number in counts)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}

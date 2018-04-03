67f4b7c1e46c3852cff16c04b4912eceda8c62eb
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-03 18:10:57 +0200

    Lists - Lab

---
+++ 6.0ListLab/06.SquareNumbers/06.SquareNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.SquareNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> squareNumbers = new List<int>();
+
+            foreach (var number in numbers)
+            {
+
+                if (Math.Sqrt(number) == (int)Math.Sqrt(number))
+                //To find out whether an integer is “square number”, check whether its square root is integer number.
+                {
+                    squareNumbers.Add(number);
+                }
+                squareNumbers.Sort((a, b) => b.CompareTo(a));
+                //order the results list in descending order use sorting by lambda function:
+            }
+
+            //Print the result.
+            Console.WriteLine(String.Join(" ",squareNumbers));
+        }
+    }
+}

52ef272a768c348a89fb03cf2896bec525bbf6f8
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-04 20:31:35 +0200

    C# Intro and Basic Syntax - More Exercises

---
+++ 1.2C#IntroAndBasicSyntaxMoreExercises/01.X/01.X.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01.X
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n / 2; i++)// with this for loop we are printing the upper part of the figure.
+            {
+                var side = new string(' ', i);
+                var middle = new string(' ', n - (i * 2 + 2));
+                Console.WriteLine($"{side}x{middle}x{side}");
+            }
+
+            var center = new string(' ', n / 2);
+
+            Console.WriteLine($"{center}x{center}");
+
+            for (int i = n/2; i > 0; i--) // printing the lower part of the figure
+            {
+                var side = new string(' ', i - 1);
+                var middle = new string(' ', n -2 * i);
+                Console.WriteLine($"{side}x{middle}x{side}");
+            }
+        }
+    }
+}

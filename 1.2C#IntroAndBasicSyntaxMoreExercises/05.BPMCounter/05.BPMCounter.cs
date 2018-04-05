ed81afece4d2d66e6a9de0815e2ea350512b8dd7
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-05 22:04:37 +0200

    C# Intro and Basic Syntax - More Exercises

---
+++ 1.2C#IntroAndBasicSyntaxMoreExercises/05.BPMCounter/05.BPMCounter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _05.BPMCounter
+{
+    class Program
+    {
+        static void Main()
+        {
+            double beatsPerMinute = double.Parse(Console.ReadLine());
+            double numberOfBeats = double.Parse(Console.ReadLine());
+            double bars = numberOfBeats / 4;
+            double seconds = numberOfBeats * 60 / beatsPerMinute;
+            int minutes = (int)seconds / 60;
+            seconds -= minutes * 60;
+
+            Console.WriteLine($"{Math.Round(bars,1)} bars - {minutes}m {Math.Floor(seconds)}s ");
+            
+
+        }
+    }
+}

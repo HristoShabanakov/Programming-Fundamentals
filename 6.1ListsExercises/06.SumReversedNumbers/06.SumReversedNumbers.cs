684753fc30963ad3572d9e50c7cca355958d028f
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-06 23:03:33 +0200

    Lists - Exercises

---
+++ 6.1ListsExercises/06.SumReversedNumbers/06.SumReversedNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.SumReversedNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            long result = Console.ReadLine()
+            .Split()
+            .Select(a => int.Parse(new string(a.Reverse()
+            .ToArray())))
+            .Sum();
+
+            Console.WriteLine(result);
+
+        }
+    }
+}

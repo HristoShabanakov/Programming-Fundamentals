7ab1566f34f3cca243fe74546c1d1ab67c8476b5
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-06 22:50:22 +0200

    Lists - Exercises

---
+++ 6.1ListsExercises/03.SearchForANumber/03.SearchForANumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.SearchForANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> commands = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            //.Take method allows you to get the first several elements from a sequence.
+            List<int> result = numbers.Take(commands[0]).ToList();
+
+            //.RemoveRange it will remove all elements starting from Zero, until the number we enter from the console "commands".
+            result.RemoveRange(0, commands[1]);
+
+            //.Contains is an instance method—you can call it on a specific string in your program. 
+            //It has a bool result, which is true if the parameter is found, and false if it is not found.
+            Console.WriteLine(result.Contains(commands[2]) ? "YES!" : "NO!");
+             
+
+            
+        }
+    }
+}

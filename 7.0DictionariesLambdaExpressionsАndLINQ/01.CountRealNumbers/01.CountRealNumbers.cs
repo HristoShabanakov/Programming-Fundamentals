cf980773ccb996c2259c358124703532500a3a66
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-14 15:07:07 +0200

    Dictionaries - Lab

---
+++ 7.0DictionariesLambdaExpressionsАndLINQ/01.CountRealNumbers/01.CountRealNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountRealNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            double[] input = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            SortedDictionary<double,int> counts = new SortedDictionary<double, int>();
+
+            foreach (var number in input)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+
+                else
+                {
+                    counts[number] = 1;
+                }
+            }
+
+            foreach (var number in counts.Keys)
+            {
+                Console.WriteLine($"{number} -> {counts[number]}");
+            }
+
+        }
+    }
+}

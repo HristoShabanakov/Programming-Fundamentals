552ae560c929b7a8ff07ec23bac9eb069c031d78
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-15 13:38:01 +0200

    LINQ - Lab

---
+++ 7.0DictionariesLambdaExpressionsАndLINQ/03.MinMaxSumAverage/03.MinMaxSumAverage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.MinMaxSumAverage
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(nums.Sum());
+            Console.WriteLine(nums.Max());
+            Console.WriteLine(nums.Min());
+            Console.WriteLine(nums.Average());
+        }
+    }
+}

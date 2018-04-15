242f6fe9dd8e2add6cbfa43b9fff5d61100a4420
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-15 14:36:09 +0200

    LINQ - Lab

---
+++ 7.0DictionariesLambdaExpressionsАndLINQ/05.ShortWordsSorted/05.ShortWordsSorted.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.ShortWordsSorted
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            List<string> words = Console.ReadLine()
+                .ToLower()
+                .Split(", ; : . ! ( ) \" ' \\ / [ ] "
+                .ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            words = words.Where(s => s.Length < 5)
+                .OrderBy(x => x)
+                .Distinct()
+                .ToList();
+
+            Console.WriteLine(string.Join(", " ,words));
+        }
+    }
+}

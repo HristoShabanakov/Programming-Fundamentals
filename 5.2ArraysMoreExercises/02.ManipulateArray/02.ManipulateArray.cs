2f850f01ee8d9946a187d2f87bcfdad0b8351174
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-16 20:23:05 +0200

    Arrays - More Exercises

---
+++ 5.2ArraysMoreExercises/02.ManipulateArray/02.ManipulateArray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.ManipulateArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            var words = Console.ReadLine().Split(' ').ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split();
+
+                switch (input[0])
+                {
+                    case "Reverse":
+                        words = words.Reverse().ToArray();
+                        break;
+
+                    case "Distinct":
+                       words = words.Distinct().ToArray();
+                        break;
+
+                    case "Replace":
+                        var index = int.Parse(input[1]);
+                        var word = input[2];
+                        words[i] = word;
+                        break;
+                }
+
+            }
+            Console.WriteLine(string.Join(" ", words));
+
+        }
+    }
+}

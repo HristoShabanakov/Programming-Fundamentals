02c589a2ed2242f49ff329f865c1a00abbca7d9e
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-16 20:22:19 +0200

    Arrays - More Exercises

---
+++ 5.2ArraysMoreExercises/01.ArrayStatistics/01.ArrayStatistics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ArrayStatistics
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+             
+            Console.WriteLine($"Min = {numbers.Min()}");
+            Console.WriteLine($"Max = {numbers.Max()}");
+            Console.WriteLine($"Sum = {numbers.Sum()}");
+            Console.WriteLine($"Average = {numbers.Average()}");
+        }
+    }
+}

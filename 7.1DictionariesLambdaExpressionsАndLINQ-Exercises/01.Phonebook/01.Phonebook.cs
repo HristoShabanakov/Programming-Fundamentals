2d4109b8d8a2c5157c474ea0c1883daf7d630684
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-17 17:21:10 +0200

    Dictionaries Lambda Expressions and LINQ - Exercises

---
+++ 7.1DictionariesLambdaExpressionsАndLINQ-Exercises/01.Phonebook/01.Phonebook.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Phonebook
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string, string> phonebook = new Dictionary<string, string>();
+
+            while (!input.Equals("END"))
+            {
+                string[] phonebookEntry = Console.ReadLine().Split(' ').ToArray();
+
+                if (phonebookEntry[0].Equals("A"))
+                {
+                    string name = phonebookEntry[1];
+                    string number = phonebookEntry[2];
+
+                    if (!phonebook.ContainsKey(name))
+                    {
+                        phonebook.Add(name, string.Empty);
+                    }
+                    phonebook[name] = number;
+                }
+
+                else
+                {
+                    string name = phonebookEntry[1];
+
+                    if (phonebook.ContainsKey(name))
+                    {
+                        Console.WriteLine($"{name} -> {phonebook[name]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {name} does not exist.");
+                    }
+                }
+                
+            }
+            input = Console.ReadLine();
+        }
+    }
+}

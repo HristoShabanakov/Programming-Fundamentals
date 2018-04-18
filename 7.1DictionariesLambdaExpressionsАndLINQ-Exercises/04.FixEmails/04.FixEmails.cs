9d7490425922c9fa7fb60f7c188157cdb5298950
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-18 22:19:57 +0200

    Dictionaries Lambda Expressions and LINQ - Exercises

---
+++ 7.1DictionariesLambdaExpressionsАndLINQ-Exercises/04.FixEmails/04.FixEmails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.FixEmails
+{
+    class Program
+    {
+        static void Main()
+        {
+            var emailList = new Dictionary<string, string>(); // name, email
+
+            while (true)
+            {
+                string name = Console.ReadLine();
+                if (name == "stop")
+                {
+                    break;
+                }
+                string email = Console.ReadLine();
+                //In a substring, we extract a fragment of an existing string.
+                // We can use the Substring method with 2 parameters—the first is 0 and the second is the desired length. 
+                string domain = email.Substring(email.Length - 2);
+                if(domain =="uk" || domain == "us")
+                {
+                    continue;
+                }
+                else
+                {
+                    emailList.Add(name, email);
+                }
+            }
+
+            foreach (var item in emailList)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}

3ae55af4784b8112ec8029a160d83ef816d0f044
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-23 22:14:39 +0200

    Objects and Classes - Lab

---
+++ 8.0ObjectsAndClassesLab/01.DayOfWeek/01.DayOfWeek.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace _01.DayOfWeek
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            //Use the method DateTime.ParseExact(string date, format, provider) to convert the input string to object of type DateTime. 
+            //Use format “d-M-yyyy” and CultureInfo.InvariantCulture.
+            DateTime myDate = DateTime.ParseExact(input, "d-M-yyyy", CultureInfo.InvariantCulture);
+
+            //The newly created DateTime object has property DayOfWeek.
+            Console.WriteLine(myDate.DayOfWeek);
+        }
+    }
+}

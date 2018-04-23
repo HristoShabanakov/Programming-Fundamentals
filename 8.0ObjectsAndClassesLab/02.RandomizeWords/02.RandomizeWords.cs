f47203241e3a93fc45b0b1e3b1cd5550cb9cd543
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-04-23 22:15:36 +0200

    Objects and Classes - Lab

---
+++ 8.0ObjectsAndClassesLab/02.RandomizeWords/02.RandomizeWords.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.RandomizeWords
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] words = Console.ReadLine().Split(' ').ToArray();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int index = rnd.Next(0, words.Length);
+                string reminder = words[index];
+                int newIndex = rnd.Next(0, words.Length);
+                words[index] = words[newIndex];
+                words[newIndex] = reminder;
+            }
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+        }
+    }
+}

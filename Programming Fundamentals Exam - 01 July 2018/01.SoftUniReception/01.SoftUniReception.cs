d306910031dec929b1693d6814a10cc3a984e0c6
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-08-14 22:40:17 +0200

    Programming Fundamentals Exam - 01 July 2018

---
+++ Programming Fundamentals Exam - 01 July 2018/01.SoftUniReception/01.SoftUniReception.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class SoftUniReception
+{
+    static void Main()
+    {
+        int employeeOne = int.Parse(Console.ReadLine());
+        int employeeTwo = int.Parse(Console.ReadLine());
+        int employeeThree = int.Parse(Console.ReadLine());
+        int studentsCount = int.Parse(Console.ReadLine());
+        int hours = 0;
+        int studentsServed = 0;
+
+        int studentsPerHour = employeeOne + employeeTwo + employeeThree;
+
+        while (studentsServed <studentsCount)
+        {
+            studentsServed += studentsPerHour;
+            hours++;
+        //Every forth hour all of the employees have a break, so they don’t work for a hour.
+            if (hours % 4 == 0)
+                hours++;
+        }
+
+        Console.WriteLine($"Time needed: {hours}h.");
+    }
+}
+

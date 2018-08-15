171bf81243cef8107d94d9d339e6898f4bb3d96b
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-08-15 23:32:10 +0200

    Exam Preparation II Arrays and Lists

---
+++ Exam Preparation II Arrays and Lists/04.HornetAssault/04.HornetAssault.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class HornetAssault
+{
+    static void Main()
+    {
+        List<int> beehives = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        List<int> hornets = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        for (int i = 0; i < beehives.Count; i++)
+        {// Every time when we are removing an element from the list its count is decreasing.
+            if (beehives[i] < hornets.Sum())
+            {
+                beehives.RemoveAt(i);
+             // In order to balance it we substract each iteration one element.
+                i--;
+            }
+            else
+            {
+                beehives[i] -= hornets.Sum();
+                if (hornets.Count == 0)
+                {
+                    Console.WriteLine(string.Join(" ",beehives));
+                    return;
+                }
+                hornets.RemoveAt(0);
+            }
+        }
+        if (beehives.Sum() > 0)
+        {
+            Console.WriteLine(string.Join(" ", beehives));
+        }
+        else
+        {
+            Console.WriteLine(string.Join(" ", hornets));
+        }
+    }
+}

605c891c9bb29ce09b14c533b776a237da76c7c3
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-08-16 23:40:48 +0200

    Programming Fundamentals Exam - 25 April 2018

---
+++ Programming Fundamentals Exam - 25 April 2018/01.RageExpenses/01.RageExpenses.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+class RageExpenses
+{
+    static void Main()
+    {
+        int lostGames = int.Parse(Console.ReadLine());
+        double headsetPrice = double.Parse(Console.ReadLine());
+        double mousePrice = double.Parse(Console.ReadLine());
+        double keyboardPrice = double.Parse(Console.ReadLine());
+        double displayPrice = double.Parse(Console.ReadLine());
+
+        int trashedHeadset = 0;
+        int trashedMouse = 0;
+        int trashedKeybord = 0;
+        int trashedDisplay = 0;
+
+        for (int i = 0; i < lostGames; i++)
+        {   //Every second lost game, Pesho trashes his headset.
+            trashedHeadset = lostGames / 2;
+            //Every third lost game, Pesho trashes his mouse.
+            trashedMouse = lostGames / 3;
+            //When Pesho trashes both his mouse and headset in the same lost game, he also trashes his keyboard
+            trashedKeybord = lostGames / 6;
+            //Every second time, when he trashes his keyboard, he also trashes his display.
+            trashedDisplay = lostGames / 12;
+
+        }
+        double total = trashedHeadset * headsetPrice + trashedMouse * mousePrice + keyboardPrice * trashedKeybord + trashedDisplay * displayPrice;
+        Console.WriteLine($"Rage expenses: {total:f2} lv.");
+    }
+}

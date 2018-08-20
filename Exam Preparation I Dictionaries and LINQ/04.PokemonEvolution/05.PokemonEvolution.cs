c0378b9b809e59fa4399966ac97ed0cee7a2159b
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-08-20 22:00:41 +0200

    Exam Preparation I Dictionaries and LINQ

---
+++ Exam Preparation I Dictionaries and LINQ/04.PokemonEvolution/05.PokemonEvolution.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class PokemonEvolution
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine()
+            .Split(new char[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
+        string name;
+        string type;
+        int index = 0;
+
+        Dictionary<string, List<string>> pokemons = new Dictionary<string, List<string>>();
+
+        while (input[0] != "wubbalubbadubdub")
+        {
+            if (input.Length > 1)
+            {
+                name = input[0];
+                type = input[1];
+                index = int.Parse(input[2]);
+                string evolution = type + " <-> " + index;
+                if (!pokemons.ContainsKey(name))
+                {
+                    List<string> current = new List<string>();
+                    current.Add(evolution);
+                    pokemons.Add(name, current);
+                }
+
+                else
+                {
+                    pokemons[name].Add(evolution);
+                }
+            }
+
+            else
+            {
+                name = input[0];
+                if (pokemons.ContainsKey(name))
+                {
+                    Console.WriteLine($"# {name}");
+                    foreach (var item in pokemons[name])
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+            input = Console.ReadLine()
+            .Split(new char[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
+        }
+        foreach (var pair in pokemons)
+        {
+            Console.WriteLine($"# {pair.Key}");
+            foreach (var ev in pair.Value.OrderByDescending(x => int.Parse(x.Split(new char[] { '-', ' ', '<', '>' }, StringSplitOptions.RemoveEmptyEntries).Last())))
+            {
+                Console.WriteLine(ev);
+            }
+        }
+    }
+}

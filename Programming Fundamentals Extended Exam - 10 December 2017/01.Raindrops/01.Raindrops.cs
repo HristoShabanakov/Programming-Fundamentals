504764e6c627450ba6adb87c8b52c867d916fd0c
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-08-22 00:56:29 +0200

    Programming Fundamentals Extended Exam - 10 December 2017

---
+++ Programming Fundamentals Extended Exam - 10 December 2017/01.Raindrops/01.Raindrops.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+
+
+class Raindrops
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        double density = double.Parse(Console.ReadLine());
+        double total = 0;
+        for (int i = 0; i < n; i++)
+        {
+            string[] regions = Console.ReadLine().Split(' ').ToArray();
+            long raindropsCount = long.Parse(regions[0]);
+            int squareMeters = int.Parse(regions[1]);
+            //We have to integers and when divided them the result will be integer, not a floating point number.
+            //So we multiply by 1.0 in order to get a floating poing number as result.
+            double regionalcoefficient = raindropsCount * 1.0 / squareMeters;
+            total += regionalcoefficient;
+        }
+        
+        if (density!=0)
+        {
+            Console.WriteLine($"{total / density:f3} ");
+        }
+        else
+        {
+            Console.WriteLine($"{total:f3}");
+        }
+    }
+}
+

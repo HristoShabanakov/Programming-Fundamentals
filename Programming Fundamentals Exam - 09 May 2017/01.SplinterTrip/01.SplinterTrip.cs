d067d84b025d07deb47af55351e886a5474fc18c
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-08-23 14:31:29 +0200

    Programming Fundamentals Exam - 09 May 2017

---
+++ Programming Fundamentals Exam - 09 May 2017/01.SplinterTrip/01.SplinterTrip.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+class SplinterTrip
+{
+    static void Main()
+    {
+        double miles = double.Parse(Console.ReadLine());
+        double capacity = double.Parse(Console.ReadLine());
+        double heavyWinds = double.Parse(Console.ReadLine());
+        double fuelConsumption = (miles - heavyWinds) * 25;
+        double heavyWindsConsumption = heavyWinds * (25 * 1.5);
+        double totalFuelConsumption = fuelConsumption + heavyWindsConsumption;
+        double tolerance = totalFuelConsumption + (totalFuelConsumption * 0.05);
+        double remainingFuel = capacity - tolerance;
+
+
+        Console.WriteLine($"Fuel needed: {tolerance:f2}L");
+        if (tolerance <= capacity)
+        {
+            Console.WriteLine($"Enough with {remainingFuel:f2}L to spare! ");
+        }
+        else
+        {
+            Console.WriteLine($"We need {Math.Abs(remainingFuel):f2}L more fuel.");
+        }
+
+    }
+}
+

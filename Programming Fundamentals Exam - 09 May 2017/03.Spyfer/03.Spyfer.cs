96f9ff10b1c0ab31ad80e53a66bf2ff227fba8d1
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-08-23 22:44:53 +0200

    Programming Fundamentals Exam - 09 May 2017

---
+++ Programming Fundamentals Exam - 09 May 2017/03.Spyfer/03.Spyfer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Spyfer
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (i == 0)
+            {
+                if(numbers[i] == numbers[i + 1])
+                {
+                    numbers.RemoveAt(i + 1);
+                 //When we remove and element we have to process again from the start,
+                 //to do that we set up i = 0 this will bring us back in the beginning of the list
+                    i = 0;
+                }
+            }
+            else if (i == numbers.Count - 1)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    numbers.RemoveAt(i - 1);
+                    i = 0;
+                }
+            }
+            //For all other cases or when i is in the middle.
+            else
+            {
+                if(numbers[i]==numbers[i-1] + numbers[i + 1])
+                {
+                    numbers.RemoveAt(i + 1);
+                    numbers.RemoveAt(i - 1);
+                    i = 0;
+                }
+            }
+        }
+        Console.WriteLine(string.Join(" ",numbers));
+    }
+}

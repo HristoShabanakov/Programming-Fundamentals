6d0788d603e3f8bcb37cfb6183415b4818abe8ba
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-01-06 22:13:11 +0100

    01.SnowBalls

---
+++ Programming Fundamentals Retake Exam - 05 January 2018/SnowBalls/SnowBalls.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+
+class SnowBalls
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int maxsnowballSnow = 0;
+        int maxsnowballTime = 0;
+        int maxsnowballQuality = 0;
+        BigInteger maxSnowballValue = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int snowballSnow = int.Parse(Console.ReadLine());
+            int snowballTime = int.Parse(Console.ReadLine());
+            int snowballQuality = int.Parse(Console.ReadLine());
+
+            BigInteger snowballValue = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality);
+
+            if (snowballValue > maxSnowballValue)
+            {
+                maxSnowballValue = snowballValue;
+                maxsnowballSnow = snowballSnow;
+                maxsnowballTime = snowballTime;
+                maxsnowballQuality = snowballQuality;
+            }
+        }
+        Console.WriteLine($"{maxsnowballSnow} : {maxsnowballTime} = {maxSnowballValue} ({maxsnowballQuality})");
+    }
+}
+

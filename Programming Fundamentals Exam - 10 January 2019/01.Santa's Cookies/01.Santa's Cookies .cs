b4d2d23193302f538c4a4b91b41dab4c24a359ad
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-01-12 10:59:29 +0100

    Problem 01 - Data Types

---
+++ Programming Fundamentals Exam - 10 January 2019/01.Santa's Cookies/01.Santa's Cookies .cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int cup = 140;
+        int smallSpoon = 10;
+        int bigSpoon = 20;
+        int singleCookieGrams = 25;
+        int cookiesPerBox = 5;
+        int totalBoxes = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int flour = int.Parse(Console.ReadLine());
+            int sugar = int.Parse(Console.ReadLine());
+            int cocoa = int.Parse(Console.ReadLine());
+
+            int flourCups = flour / cup;
+            int sugarSpoons = sugar / 20;
+            int cocoaSpoons = cocoa / smallSpoon;
+            int totalCookiesPerBake = (cup + smallSpoon + bigSpoon) * Math.Min(Math.Min(flourCups, sugarSpoons), cocoaSpoons) / singleCookieGrams;
+            int boxesOfCookies = totalCookiesPerBake / cookiesPerBox;
+            totalBoxes = boxesOfCookies + totalBoxes;
+
+            if (boxesOfCookies > 0)
+            {
+                Console.WriteLine($"Boxes of cookies: {boxesOfCookies}");
+            }
+            else
+            {
+                Console.WriteLine("Ingredients are not enough for a box of cookies.");
+            }
+            
+        }
+        Console.WriteLine($"Total boxes: {totalBoxes}");
+    }
+}
+
